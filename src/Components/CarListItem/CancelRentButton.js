bb59eaf899c6b9daa66510109f4496b0961557ce
agatapier <agata.piernikowska@yahoo.com>
2018-04-26 15:50:43 +0200

    zaimportowanie React, stworzenie klasy CancelRentButton, stworzenie button cancel reservation

---
+++ src/Components/CarListItem/CancelRentButton.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class CancelRentButton extends React.Component {
+  render () {
+    return (
+      <button>
+        Delete reservation
+      </button>
+    )
+  }
+}
+
+export default CancelRentButton
