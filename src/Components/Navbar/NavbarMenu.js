844a07050542004a9f92d0220bbda834f0ba01c4
lkoziara <lukasz.koziara88@gmail.com>
2018-04-26 16:55:34 +0200

    Navbar

---
+++ src/Components/Navbar/NavbarMenu.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import './NabarMenu.css'
+
+
+class NavbarMenu extends Component {
+
+
+  render() {
+    const  activeSection = {color: '#351C66'};
+
+    return (
+      <div className = 'menu__container'>
+
+        <div className = 'menu__button active'>Rent a car</div>
+        <div className = 'menu__button__divider'> </div>
+        <div className = 'menu__button'>My rentals</div>
+
+
+      </div>
+    )
+  }
+}
+
+
+export default NavbarMenu
