b850c5712f144279b494e02a6e914f1e0e49bcb3
agatapier <agata.piernikowska@yahoo.com>
2018-04-26 16:47:47 +0200

    dodanie checkboxow

--- src/Components/DateForm/CarFeatures/CarFeatures.js
+++ src/Components/DateForm/CarFeatures/CarFeatures.js
@@ -5,6 +5,32 @@ class CarFeatures extends React.Component {
   render () {
     return (
       <div className='CarFeatures'>
+        <input
+          type="checkbox"
+          id="Autonomous car"
+          />
+        <label for = "Autonomous car">Autonomus Car</label>
+        <input
+          type="checkbox"
+        />
+        <input
+          type="checkbox"
+        />
+        <input
+          type="checkbox"
+        />
+        <input
+          type="checkbox"
+        />
+        <input
+          type="checkbox"
+        />
+        <input
+          type="checkbox"
+        />
+        <input
+          type="checkbox"
+        />
 
       </div>
 
