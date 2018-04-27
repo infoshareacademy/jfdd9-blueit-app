c5210dd4144845044133e8e2ec35eec632053110
maciabe <maciabe@hotmail.com>
2018-04-27 17:01:01 +0200

    last one b4 vaca

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import CarRentButton from './Components/CarList/CarListItem/CarRentButton/CarRentButton';
 import ShowMore from './Components/CarList/CarListItem/ShowMore/ShowMore'
-import CarDescription from './Components/CarList/CarListItem/CarDescription/CarDescription';
 import CarImg from './Components/CarList/CarListItem/CarImg/CarImg';
 
 class App extends Component {
@@ -11,7 +10,6 @@ class App extends Component {
       <div className="App">
         <p><CarRentButton/></p>
         <p><ShowMore/></p>
-        <p><CarDescription/></p>
         <p><CarImg/></p>
       </div>
     );
--- src/Components/CarList/CarListItem/CarImg/CarImg.js
+++ src/Components/CarList/CarListItem/CarImg/CarImg.js
@@ -1,20 +1,18 @@
 import React from 'react'
-// import './CarRentButton.css'
+import './CarImg.css'
 import SUV from '../../../img/car-SUV.jpg'
 import fullsize from '../../../img/car-fullsize.jpg'
-// import compact from '../../../img/car-compact.jpg'
+import compact from '../../../img/car-compact.jpg'
 import minivan from '../../../img/car-minivan.jpg'
 
 class CarImg extends React.Component {
   render() {
     return (
       <div className="CarImg">
-        <div><img src={SUV}/></div>
-        {/*<div><img src={compact}/></div>*/}
+        <div ><img src={SUV}/></div>
+        <div><img src={compact}/></div>
         <div><img src={minivan}/></div>
         <div><img src={fullsize}/></div>
-
-
       </div>
     )
   }
