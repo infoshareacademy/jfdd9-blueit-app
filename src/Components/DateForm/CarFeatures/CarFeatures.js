ab13770fd80addfbb150d565f7172b767f3cd694
agatapier <agata.piernikowska@yahoo.com>
2018-04-27 15:49:15 +0200

    dodanie funkcjonalnosci, zmiana koloru tla i czcionki

--- src/Components/DateForm/CarFeatures/CarFeatures.js
+++ src/Components/DateForm/CarFeatures/CarFeatures.js
@@ -5,32 +5,46 @@ class CarFeatures extends React.Component {
   render () {
     return (
       <div className='CarFeatures'>
-        <input
+        <p><input
+          type="checkbox"
+          id="Family car"
+        />
+          <label for = "Family car">Family Car</label></p>
+        <p><input
+          type="checkbox"
+          id="Sports car"
+        />
+          <label for = "Sports car">Sports Car</label></p>
+        <p><input
           type="checkbox"
           id="Autonomous car"
           />
-        <label for = "Autonomous car">Autonomus Car</label>
-        <input
+          <label for = "Autonomous car">Autonomus Car</label></p>
+        <p><input
           type="checkbox"
+          id="Child care"
         />
-        <input
+          <label for = "Child care">Child care</label></p>
+        <p><input
           type="checkbox"
+          id="Pet friendly"
         />
-        <input
+          <label for = "Pet friendly">Pet friendly</label></p>
+        <p><input
           type="checkbox"
+          id="Tour guide"
         />
-        <input
+          <label for = "Tour guide">Tour guide</label></p>
+        <p><input
           type="checkbox"
+          id="Cable TV"
         />
-        <input
+          <label for = "Cable TV">Cable TV</label></p>
+        <p><input
           type="checkbox"
+          id="Private bathroom"
         />
-        <input
-          type="checkbox"
-        />
-        <input
-          type="checkbox"
-        />
+          <label for = "Private bathroom">Private bathroom</label></p>
 
       </div>
 
