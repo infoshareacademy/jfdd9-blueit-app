b565a6a34b5b6288f4307934c0c9e43d2852f5d9
agatapier <agata.piernikowska@yahoo.com>
2018-05-07 16:44:46 +0200

    zmiana for na htmlFor

--- src/Components/DateForm/CarFeatures/CarFeatures.js
+++ src/Components/DateForm/CarFeatures/CarFeatures.js
@@ -9,42 +9,42 @@ class CarFeatures extends React.Component {
           type="checkbox"
           id="Family car"
         />
-          <label for = "Family car">Family Car</label></p>
+          <label htmlFor="Family car">Family Car</label></p>
         <p><input
           type="checkbox"
           id="Sports car"
         />
-          <label for = "Sports car">Sports Car</label></p>
+          <label htmlFor="Sports car">Sports Car</label></p>
         <p><input
           type="checkbox"
           id="Autonomous car"
           />
-          <label for = "Autonomous car">Autonomus Car</label></p>
+          <label htmlFor="Autonomous car">Autonomus Car</label></p>
         <p><input
           type="checkbox"
           id="Child care"
         />
-          <label for = "Child care">Child care</label></p>
+          <label htmlFor= "Child care">Child care</label></p>
         <p><input
           type="checkbox"
           id="Pet friendly"
         />
-          <label for = "Pet friendly">Pet friendly</label></p>
+          <label htmlFor="Pet friendly">Pet friendly</label></p>
         <p><input
           type="checkbox"
           id="Tour guide"
         />
-          <label for = "Tour guide">Tour guide</label></p>
+          <label htmlFor="Tour guide">Tour guide</label></p>
         <p><input
           type="checkbox"
           id="Cable TV"
         />
-          <label for = "Cable TV">Cable TV</label></p>
+          <label htmlFor="Cable TV">Cable TV</label></p>
         <p><input
           type="checkbox"
           id="Private bathroom"
         />
-          <label for = "Private bathroom">Private bathroom</label></p>
+          <label htmlFor="Private bathroom">Private bathroom</label></p>
 
       </div>
 
