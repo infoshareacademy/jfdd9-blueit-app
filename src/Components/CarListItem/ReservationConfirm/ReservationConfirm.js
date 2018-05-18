3d7af3b75ef5b981ff2f9cbbbbf7971db82c929b
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-05-18 14:24:46 +0200

    Input for choosing place in confirmation reservation screen

--- src/Components/CarListItem/ReservationConfirm/ReservationConfirm.js
+++ src/Components/CarListItem/ReservationConfirm/ReservationConfirm.js
@@ -53,47 +53,56 @@ class ReservationConfirm extends Component {
   render() {
     return (
       <Fragment>
-        <p>{this.props.carId}</p>
+        <form onSubmit={this.handleSubmit}>
 
-        <div className="datePicker__container">
-          <DatePicker
-            className="RentDateForm"
-            locale="en-gb"
-            dateFormat="YYYY/MM/DD"
-            placeholderText="Start date"
-            todayButton={"Today"}
-            minDate={moment()}
-            maxDate={moment().add(1, "month")}
-            selected={this.state.startDate}
-            selectsStart
-            startDate={this.state.startDate}
-            endDate={this.state.endDate}
-            onChange={this.handleChangeStartDate}
-            // withPortal
-            fixedHeight
-          />
+          <div className="datePicker__container">
+            <DatePicker
+              className="RentDateForm"
+              locale="en-gb"
+              dateFormat="YYYY/MM/DD"
+              placeholderText="Start date"
+              todayButton={"Today"}
+              minDate={moment()}
+              maxDate={moment().add(1, "month")}
+              selected={this.state.startDate}
+              selectsStart
+              startDate={this.state.startDate}
+              endDate={this.state.endDate}
+              onChange={this.handleChangeStartDate}
+              // withPortal
+              fixedHeight
+            />
 
-          <DatePicker
-            className="RentDateForm"
-            locale="en-gb"
-            dateFormat="YYYY/MM/DD"
-            placeholderText="End date"
-            minDate={moment(this.state.startDate)}
-            maxDate={moment(this.state.startDate).add(14, "days")}
-            selected={this.state.endDate}
-            selectsEnd
-            startDate={this.state.startDate}
-            endDate={this.state.endDate}
-            onChange={this.handleChangeEndDate}
-            disabled={this.isStartDateEmpty()}
-            // withPortal
-            fixedHeight
-          >
-            <div className="CalendarDateTo">
-              Maximum rent period is 14 days
-            </div>
-          </DatePicker>
-        </div>
+            <DatePicker
+              className="RentDateForm"
+              locale="en-gb"
+              dateFormat="YYYY/MM/DD"
+              placeholderText="End date"
+              minDate={moment(this.state.startDate)}
+              maxDate={moment(this.state.startDate).add(14, "days")}
+              selected={this.state.endDate}
+              selectsEnd
+              startDate={this.state.startDate}
+              endDate={this.state.endDate}
+              onChange={this.handleChangeEndDate}
+              disabled={this.isStartDateEmpty()}
+              // withPortal
+              fixedHeight
+            >
+              <div className="CalendarDateTo">
+                Maximum rent period is 14 days
+              </div>
+            </DatePicker>
+          </div>
+
+          <div className="PlaceContainer">
+            <input
+              placeholder="Choose the rent place"
+              className="RentDateForm"
+            />
+          </div>
+
+        </form>
       </Fragment>
     )
   }
