2fbffde1d65514d1e8103d1d2901f4d3ea35ba77
maciabe <maciabe@hotmail.com>
2018-05-19 00:44:01 +0200

    WIP styling confirmation screen

--- src/Components/UserAddedCars/UserAddedCars.js
+++ src/Components/UserAddedCars/UserAddedCars.js
@@ -1,6 +1,7 @@
 import React, {Component, Fragment} from 'react'
 import CarFeatures from '../CarFeatures/CarFeatures'
 import firebase from 'firebase'
+import './UserAddedCars.css'
 
 
 class UserAddedCars extends React.Component {
@@ -18,7 +19,7 @@ class UserAddedCars extends React.Component {
   })
 
   addCar = () => {
-    const { cars, ...rest} = this.state
+    const {cars, ...rest} = this.state
     this.setState({
       cars: this.state.cars.concat(rest)
     })
@@ -49,50 +50,79 @@ class UserAddedCars extends React.Component {
   render() {
     return (
       <Fragment>
-        <h1>Please fill in all fields</h1>
-        <form onSubmit={this.handleSubmit} data-testid="car-form">
-          {this.state.formError && <p data-testid="car-error">{this.state.formError.message}</p>}
-          <h4>Car Type:</h4>
-          <input
-            data-testid="carType-input"
-            name="carType"
-            value={this.state.carType}
-            onChange={this.handleChange}
-            placeholder="suv, minivan,fullsize,compact"
-          />
-          <h4>Car make:</h4>
-          <input
-            data-testid="carMake-input"
-            name="carMake"
-            value={this.state.carMake}
-            onChange={this.handleChange}
-            placeholder="car maker"
-          />
-          <h4>Car model:</h4>
-          <input
-            data-testid="carModel-input"
-            name="carModel"
-            value={this.state.carModel}
-            onChange={this.handleChange}
-            placeholder="enter car model"
-          />
-          <h4>Car make year:</h4>
-          <input
-            data-testid="carYear-input"
-            name="taskDescription"
-            value={this.state.carYear}
-            onChange={this.handleChange}
-            placeholder="enter year XXXX-format"
-          />
-          <CarFeatures selectedOptions={this.state.selectedOptions} toggleOption={this.handleOptionChange}/>,
-          <button>ADD YOUR VEHICLE</button>
-        </form>
-
-        {
-          this.state.cars.map(
-            car => <p>{car.carType}</p>
-          )
-        }
+        <div className="UserAddedCarsContainer">
+          <h1>Please fill in all fields</h1>
+          <form onSubmit={this.handleSubmit} data-testid="car-form">
+            {this.state.formError && <p data-testid="car-error">{this.state.formError.message}</p>}
+            <h4>Car Type:</h4>
+            {/*<select className="UserAddedCarsSelector" required>*/}
+              {/*<option>choose one</option>*/}
+              {/*<option>SUV</option>*/}
+              {/*<option>minivan</option>*/}
+              {/*<option>fullsize</option>*/}
+              {/*<option>compact</option>*/}
+            {/*</select>*/}
+            <input
+              className="UserAddedInput"
+              data-testid="carType-input"
+              name="carType"
+              value={this.state.carType}
+              onChange={this.handleChange}
+              placeholder="choose between: SUV, minivan, fullsize, compact"
+            />
+            <h4>Car make:</h4>
+            <input
+              className="UserAddedInput"
+              data-testid="carMake-input"
+              name="carMake"
+              value={this.state.carMake}
+              onChange={this.handleChange}
+              placeholder="enter car maker"
+            />
+            <h4>Car model:</h4>
+            <input
+              className="UserAddedInput"
+              data-testid="carModel-input"
+              name="carModel"
+              value={this.state.carModel}
+              onChange={this.handleChange}
+              placeholder="enter car model"
+            />
+            <h4>Car make year:</h4>
+            <input
+              className="UserAddedInput"
+              data-testid="carYear-input"
+              name="carYear"
+              value={this.state.carYear}
+              onChange={this.handleChange}
+              placeholder="enter year in XXXX-format"
+            />
+            <CarFeatures selectedOptions={this.state.selectedOptions} toggleOption={this.handleOptionChange}/>,
+            <button className="UserAddedCarsBtn">ADD YOUR VEHICLE</button>
+          </form>
+          <div className="UserAddedCarsConfirmation">
+            {
+              this.state.cars.map(
+                car => <p>{car.carType}</p>
+              )
+            }
+            {
+              this.state.cars.map(
+              car => <p>{car.carMake}</p>
+              )
+            }
+            {
+              this.state.cars.map(
+                car => <p>{car.carModel}</p>
+              )
+            }
+            {
+              this.state.cars.map(
+                car => <p>{car.carYear}</p>
+              )
+            }
+          </div>
+        </div>
       </Fragment>
     )
   }
