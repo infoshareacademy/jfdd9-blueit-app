8e8475e2d5a5666a568b25400ebd8c7ae001dbd2
maciabe <maciabe@hotmail.com>
2018-05-21 15:51:46 +0200

    firebase connected

--- src/Components/UserAddedCars/UserAddedCars.js
+++ src/Components/UserAddedCars/UserAddedCars.js
@@ -15,13 +15,22 @@ class UserAddedCars extends React.Component {
   }
 
   handleOptionChange = option => this.setState({
-    selectedOptions: this.state.selectedOptions.concat(option)
+    selectedOptions: this.state.selectedOptions.includes(option) ?
+      this.state.selectedOptions.filter(item => item !== option) :
+      this.state.selectedOptions.concat(option)
   })
 
   addCar = () => {
     const {cars, ...rest} = this.state
-    this.setState({
-      cars: this.state.cars.concat(rest)
+    // this.setState({
+    //   cars: this.state.cars.concat(rest)
+    // })
+    firebase.database().ref('/cars').push({
+      carbody: this.state.carType,
+      make: this.state.carMake,
+      model: this.state.carModel,
+      productionYear: this.state.carYear,
+      features: this.state.selectedOptions
     })
   }
 
@@ -51,25 +60,31 @@ class UserAddedCars extends React.Component {
     return (
       <Fragment>
         <div className="UserAddedCarsContainer">
-          <h1>Please fill in all fields</h1>
+          <h1>Here is an option to add your own vehicle to our fleet</h1>
+          <h2>Please fill in all fields thoroughly:</h2>
           <form onSubmit={this.handleSubmit} data-testid="car-form">
             {this.state.formError && <p data-testid="car-error">{this.state.formError.message}</p>}
             <h4>Car Type:</h4>
-            <select className="UserAddedCarsSelector" required>
+            <select className="UserAddedCarsSelector" required
+                    value={this.state.carType}
+                    onChange={this.handleChange}
+                    name="carType"
+            >
               <option>choose one</option>
               <option>SUV</option>
               <option>minivan</option>
               <option>fullsize</option>
               <option>compact</option>
             </select>
-            <input
-              className="UserAddedInput"
-              data-testid="carType-input"
-              name="carType"
-              value={this.state.carType}
-              onChange={this.handleChange}
-              placeholder="choose between: SUV, minivan, fullsize, compact"
-            />
+            {/*<input*/}
+              {/*className="UserAddedInput"*/}
+              {/*data-testid="carType-input"*/}
+              {/*name="carType"*/}
+              {/*value={this.state.carType}*/}
+              {/*onChange={this.handleChange}*/}
+              {/*placeholder="choose between: SUV, minivan, fullsize, compact"*/}
+              {/*required*/}
+            {/*/>*/}
             <h4>Car make:</h4>
             <input
               className="UserAddedInput"
@@ -78,6 +93,7 @@ class UserAddedCars extends React.Component {
               value={this.state.carMake}
               onChange={this.handleChange}
               placeholder="enter car maker"
+              required
             />
             <h4>Car model:</h4>
             <input
@@ -87,15 +103,18 @@ class UserAddedCars extends React.Component {
               value={this.state.carModel}
               onChange={this.handleChange}
               placeholder="enter car model"
+              required
             />
             <h4>Car make year:</h4>
             <input
+              type="number"
               className="UserAddedInput"
               data-testid="carYear-input"
               name="carYear"
               value={this.state.carYear}
               onChange={this.handleChange}
-              placeholder="enter year in XXXX-format"
+              placeholder="enter year in xxxx -format"
+              required
             />
             <CarFeatures selectedOptions={this.state.selectedOptions} toggleOption={this.handleOptionChange}/>,
             <button className="UserAddedCarsBtn">ADD YOUR VEHICLE</button>
@@ -104,22 +123,14 @@ class UserAddedCars extends React.Component {
 
             {
               this.state.cars.map(
-                car => <p><strong>type: </strong>{car.carType}</p>
-              )
-            }
-            {
-              this.state.cars.map(
-              car => <p><strong>make: </strong>{car.carMake}</p>
-              )
-            }
-            {
-              this.state.cars.map(
-                car => <p><strong>model: </strong>{car.carModel}</p>
-              )
-            }
-            {
-              this.state.cars.map(
-                car => <p><strong>year: </strong>{car.carYear}</p>
+                car =>
+                  <Fragment>
+                    <p><strong>type: </strong>{car.carType}</p>
+                    <p><strong>make: </strong>{car.carMake}</p>
+                    <p><strong>model: </strong>{car.carModel}</p>
+                    <p><strong>year: </strong>{car.carYear}</p>
+                    <p><strong>features: </strong>{car.selectedOptions}</p>
+                  </Fragment>
               )
             }
           </div>
