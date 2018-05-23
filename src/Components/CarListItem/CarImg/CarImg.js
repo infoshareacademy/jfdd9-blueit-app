b83069a36ec0647ec85a496c7b5c3293a0aaf301
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-05-23 21:12:25 +0200

    Hiding Show more, Show less button if filtered cars number is less than 5

--- src/Components/CarListItem/CarImg/CarImg.js
+++ src/Components/CarListItem/CarImg/CarImg.js
@@ -26,13 +26,18 @@ class CarImg extends React.Component {
           )
         }
 
-        <div className="ShowMore">
-          <button id="show"
-                  className={this.clickHeandler.includes ? 'ShowMore' : 'ShowLess'}
-                  onClick={this.clickHeandler}
-          >
-            {this.state.buttonText}</button>
-        </div>
+        {
+          this.props.cars.length > 5 ?
+            <div className="ShowMore">
+              <button id="show"
+                      className={this.clickHeandler.includes ? 'ShowMore' : 'ShowLess'}
+                      onClick={this.clickHeandler}
+              >
+                {this.state.buttonText}</button>
+            </div> :
+            undefined
+        }
+
 
         {
           this.state.showMore && this.props.cars.slice(5, this.state.showMore ? undefined : 5).map(
