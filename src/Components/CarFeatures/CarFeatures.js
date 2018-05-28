f9a17b1c707540b88fab8d0b41c9e9075971e9d0
Agata Piernikowska <agata.piernikowska@yahoo.com>
2018-05-28 19:28:06 +0200

    dodanie More i Hide do car features

--- src/Components/CarFeatures/CarFeatures.js
+++ src/Components/CarFeatures/CarFeatures.js
@@ -16,25 +16,39 @@ const options = [
 
 
 
+
 class CarFeatures extends React.Component {
 
   state = {
-    expanded: false
+    expanded: false,
+    buttonText: 'MORE'
   }
+
+  clickHeandler = () => {
+    this.state.expanded ?
+      this.setState({expanded: false, buttonText: 'MORE'})
+      :
+      this.setState({expanded: true, buttonText: 'HIDE'})
+  }
+
   render() {
     return (
       <Fragment>
       <div className='CarFeatures'>
         <div>
-        <button onClick={() => this.setState({ expanded: true })}>MORE</button>
+        <button onClick={this.clickHeandler}
+                className={this.clickHeandler.includes ? 'MORE' : 'HIDE'}
+                >
+          {this.state.buttonText}</button>
         </div>
-<div>
+<div className="CarFeaturesOptions">
         {this.state.expanded && options.map(
           option => (
             <p key={option}
                className="option"
                style={{ background: this.props.selectedOptions.includes(option) ? 'rgb(59, 65, 122)' : undefined, cursor: 'pointer', borderRadius: 5}}
                onClick={() => this.props.toggleOption(option)}
+
             >
 
               {option}
